a9d312f001dfb6532ce7638f54a48348b34f576f
John Jones <john@johnjonesfour.com>
2022-04-11 11:45:34 -0400

    initial commit

--- build_push.sh
+++ build_push.sh
@@ -3,7 +3,7 @@
 PROJECT="$1"
 TAG="ghcr.io/johnjones4/backup-$PROJECT"
 
-docker build -t ${TAG} "./$PROJECT"
+docker build -t ${TAG} "./apps/$PROJECT"
 docker push ${TAG}:latest
 docker image rm ${TAG}:latest
 
