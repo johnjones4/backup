969731c175be00d7e9989837c46e507c86c0c502
John Jones <john@johnjonesfour.com>
2022-09-26 20:35:33 -0400

    docker backup

--- apps/backup/run.sh
+++ apps/backup/run.sh
@@ -4,12 +4,25 @@ export PGPASSWORD="$POSTGRES_PASSWORD"
 
 while :
 do
+  echo "Backing up docker"
+  mkdir /backup/docker
+  DOCKER_IDS=$(docker ps -q)
+  for ID in $DOCKER_IDS
+  do
+    docker inspect $ID > /backup/docker/$ID.json
+    if [ "$?" != "0" ]; then
+      exit 1
+    fi
+  done
+
+  echo "Backing up files"
   tar zcvf /backup/apps.tar.gz /apps
   if [ "$?" != "0" ]; then
     exit 1
   fi
+
   while read d; do
-    echo "Backing up $d"
+    echo "Backing up database $d"
     rm /backup/"$d".sql
     rm /backup/"$d".sql.gz
     pg_dump "$d" -U $POSTGRES_USER -h $POSTGRES_HOST > /backup/"$d".sql
@@ -22,5 +35,6 @@ do
     fi
     echo "Done"
   done </dbs.txt
+
   sleep "$DELAY_DAILY"
 done
